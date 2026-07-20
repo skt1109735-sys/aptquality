a22ff0f64b4c15269d32fdae6492958694f1ce00
aptquality-deploy <deploy@aptquality.local>
2026-07-20 12:51:53 +0900

    Deploy: real apartment complexes with real cadastral polygons

---
+++ _next/static/jzbErE2Iul437P1zRIFXE/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/aptquality//_next/:path+",
+        "destination": "/aptquality/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
